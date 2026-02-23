8e293fed143f0fe538299f2a60ee93f5c8307824
Kuruzukel <michael333.jc@gmail.com>
2026-02-23 21:21:02 +0800

    style(admin): Reorganize trainee action menu and update modal labels
    - Reorder dropdown menu items to prioritize View Details action
    - Update action menu labels for clarity (View → View Details, Edit → Edit Details)
    - Change edit modal title from "Trainee Details" to "Edit Trainee Details"
    - Improve UX by presenting view action before edit in the dropdown menu

--- admin/src/assets/js/accounts.js
+++ admin/src/assets/js/accounts.js
@@ -294,11 +294,11 @@ function createTraineeRow(trainee, index) {
                     <i class="bx bx-dots-vertical-rounded"></i>
                 </button>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="javascript:void(0);" onclick="editTrainee('${trainee._id}')">
-                        <i class="bx bx-edit-alt me-1"></i> Edit
-                    </a>
                     <a class="dropdown-item" href="javascript:void(0);" onclick="viewTrainee('${trainee._id}')">
-                        <i class="bx bx-show me-1"></i> View
+                        <i class="bx bx-show me-1"></i> View Details
+                    </a>
+                    <a class="dropdown-item" href="javascript:void(0);" onclick="editTrainee('${trainee._id}')">
+                        <i class="bx bx-edit-alt me-1"></i> Edit Details
                     </a>
                     <a class="dropdown-item text-danger" href="javascript:void(0);" onclick="deleteTrainee('${trainee._id}')">
                         <i class="bx bx-trash me-1"></i> Delete
